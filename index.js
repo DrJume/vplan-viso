2922299e74615bfedda50d92293ddba8cebb3ed5
DrJume <DrJume@users.noreply.github.com>
2018-02-25 21:41:58 +0100

    Removed handling invalid upload in the UploadWatcher callback

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 process.env.NODE_PATH = __dirname
 require('module')._initPaths() // Might break in the future
 
-const logger = require('helpers/logger') // eslint-disable-line import/no-unresolved
+const logger = require('helpers/logger')
 
 global.log = logger
 
-const UploadWatcher = require('services/UploadWatcher') // eslint-disable-line import/no-unresolved
+const UploadWatcher = require('services/UploadWatcher')
 
-UploadWatcher((day = 'unknown', filename) => {
-  if (day === 'unknown') {
-    return
-  }
-
-  log.info(`${day}: ${filename}`)
+UploadWatcher((day, filePath) => {
+  log.info(`${day}: ${filePath}`)
 })
