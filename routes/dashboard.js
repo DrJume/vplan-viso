f0d9f6ff09e5199be2bf9bff9003b9844382f594
DrJume <DrJume@users.noreply.github.com>
2018-03-04 21:59:19 +0100

    Added dashboard test view - using self-made express view engine

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Dashboard')
+  res.render('dashboard')
 })
 
 router.get('/settings', (req, res) => {
